bf2b950fb0447d53833cb4d163bdd76553da4136
yuehao <83541408@qq.com>
2016-10-24 19:06:51 +0800

    项目集成logback日志管理

--- src/main/java/com/caimao/controller/AnalystController.java
+++ src/main/java/com/caimao/controller/AnalystController.java
@@ -3,6 +3,7 @@ package com.caimao.controller;
 import com.caimao.entity.AnalystEntity;
 import com.caimao.service.AnalystService;
 import com.google.gson.Gson;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,10 +26,13 @@ public class AnalystController {
     @Resource
     private AnalystService analystService;
 
+    private org.slf4j.Logger logger = LoggerFactory.getLogger(AnalystController.class);
+
     @RequestMapping(value = "/info", method = RequestMethod.GET,produces = "text/html;charset=UTF-8")
     @ResponseBody
     public String first(final HttpServletResponse response, final HttpServletRequest request) throws Exception {
         List<AnalystEntity> analystEntities = analystService.queryAnalystlist();
+        logger.info(new Gson().toJson(analystEntities) == null ? "" : new Gson().toJson(analystEntities));
         response.setCharacterEncoding("UTF-8");
         return new Gson().toJson(analystEntities) == null ? "" : new Gson().toJson(analystEntities);
     }
